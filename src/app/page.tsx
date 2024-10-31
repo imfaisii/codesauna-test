b6f1099996bf61a09f8f94b92eb5dd6ff6aab011
imfaisii <>
2024-10-31 20:13:37 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ResultPage from './screen/ResultPage/page'
+
+const Home = () => {
+  return (
+    <>
+    <ResultPage />
+  </>
+  )
+}
+
+export default Home
---
+++ src/app/types/ScoreItemProps.ts
@@ -0,0 +1,6 @@
+export interface ScoreData {
+    category: string;
+    score: number;
+    icon: string;
+    categories:string[];
+  }
